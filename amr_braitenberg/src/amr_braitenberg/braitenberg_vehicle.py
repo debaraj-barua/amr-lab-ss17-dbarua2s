d4c198555517ad65c7eb421d92e468294a622999
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-04-20 21:25:25 +0200

    Fixed functionality with reference to normalized sonar readings and included comments

--- amr_braitenberg/src/amr_braitenberg/braitenberg_vehicle.py
+++ amr_braitenberg/src/amr_braitenberg/braitenberg_vehicle.py
@@ -20,6 +20,8 @@ class BraitenbergVehicle:
         self._f_1, self._f_2 = factor_1, factor_2
     
     
+
+
     def compute_wheel_speeds(self, left_in, right_in):
         """
         ==================== YOUR CODE HERE ====================
@@ -34,36 +36,51 @@ class BraitenbergVehicle:
         (in interval [0..1])
         ========================================================
         """
+        """
+        This function gets left and right sonar range readings and calculate wheel speed
+        based on the type of Braitenberg vehicle.
+        
+        @c ws1, ws2: These variables store wheel speeds for left and right wheels respectively.
+        
+        @c max_range: This variable stores maximum range of the sonar sensor
+        
+        @c normalized_left,normalized_right:
+                    This variable is used to store normalized sensor readings 
+                    scaled by maximum range.
+                    For normalized_left, this is calculated by: 
+                            (max_range-left_in)/max_range
+                    This will result in a value between 0 and 1. 
+                    
+        """
+        
         ws1=0.0
         ws2=0.0
+        max_range=5
+        normalized_left=(max_range-left_in)/max_range       
+        normalized_right=(max_range-right_in)/max_range     
+
         if (self._vehicle_type==0):
             
-            print "0000000000000000000000000000000000"
-            print "left_in: ",left_in
-            print "right_in: ",right_in
-            ws1=0.0
-            ws2=0.0   
+            ws1=normalized_left*self._f_1
+            ws2=normalized_right*self._f_2
 
         elif (self._vehicle_type==1):
-            print "1111111111111111111111111111111111"
-            print "left_in: ",left_in
-            print "right_in: ",right_in
-            ws1=0.0
-            ws2=0.0   
+           
+            ws1=normalized_right*self._f_1
+            ws2=normalized_left*self._f_2 
 
         elif (self._vehicle_type==2):
-            print "2222222222222222222222222222222222"
-            print "left_in: ",left_in
-            print "right_in: ",right_in
-            ws1=0.0
-            ws2=0.0   
+            
+            ws1=(normalized_right+normalized_left)*self._f_1
+            ws2=(normalized_right+normalized_left)*self._f_2 
 
         else:
-            print "ELSEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
-            print "left_in: ",left_in
-            print "right_in: ",right_in            
+            """
+            wheel speed zero if parameter of vehicle type is other then 0, 1 or 2
+            Generally will not execute, but included as a redundant condition
+            """
             ws1=0.0
             ws2=0.0   
-
+            
 
         return (ws1, ws2)
