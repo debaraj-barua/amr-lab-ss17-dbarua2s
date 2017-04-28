8106d99556944423236811a8500e6033f6366a58
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-04-28 19:45:45 +0200

    fixed bugs for omnidirectional motion

--- amr_navigation/src/amr_navigation/omni_velocity_controller.py
+++ amr_navigation/src/amr_navigation/omni_velocity_controller.py
@@ -52,34 +52,54 @@ class OmniVelocityController(VelocityController):
             linear_dist = get_distance(self._target_pose, actual_pose)
             angular_dist = get_shortest_angle(self._target_pose.theta, actual_pose.theta)
             
+             #calculate distance for fixed velocity and deaccelerated velocity
+            dist_accelerated=(self._a_max_vel*self._a_max_vel)/(2*self._max_l_acc)            
+            dist_nonaccelerated =  linear_dist - dist_accelerated
+
             if (    abs(linear_dist)<self._l_tolerance and
                     abs(angular_dist)<self._a_tolerance):
                 self._linear_complete = True
                 self._angular_complete = True
                 return Velocity()
-                         
+
             # Step 2: compute velocities
             linear_vel_x, linear_vel_y, angular_vel = 0, 0, 0
+            
+
+            #Non accelerated motoion:
+            l_time=linear_dist/self._l_max_vel
+            a_time=angular_dist/self._a_max_vel      
+            
+            
+            
+                         
           #  w_turn = ((pose_theta-angular_dist)/angular_dist)
             
             l_time=linear_dist/self._l_max_vel
             a_time=angular_dist/self._a_max_vel      
-            
        
+            if (a_time<l_time):
+                scaled_a_vel=self._a_max_vel*(a_time/l_time)
+                scaled_l_vel=self._l_max_vel
+            else :
+                scaled_a_vel=self._a_max_vel
+                scaled_l_vel=self._l_max_vel*(l_time/a_time)             
+            
+            
             
             
             if abs(linear_dist)>self._l_tolerance:
-                scaled_l_vel=self._l_max_vel
+                
                 linear_vel_x = (scaled_l_vel*cos(pose_theta) if abs(linear_dist)>5*self._l_tolerance else
                                self._l_tolerance*cos(pose_theta))
                 linear_vel_y = (scaled_l_vel*sin(pose_theta) if abs(linear_dist)>5*self._l_tolerance else
                                self._l_tolerance*sin(pose_theta))
             if abs(angular_dist)>self._a_tolerance:
-                scaled_a_vel=self._a_max_vel*(a_time/l_time)
+                
                       
                 angular_vel=(scaled_a_vel if abs(angular_dist)>5*self._a_tolerance else
                                self._a_tolerance*(a_time/l_time))
-               
+             
             rospy.loginfo(actual_pose)
             return Velocity(linear_vel_x,
                             linear_vel_y,
